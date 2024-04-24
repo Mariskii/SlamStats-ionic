593327447b7ee16cc8f29d7643b29b8f81f062a5
Vicent Paya <vicentpayaginer@gmail.com>
2024-04-24 12:16:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'slamstats',
+  webDir: 'www'
+};
+
+export default config;
