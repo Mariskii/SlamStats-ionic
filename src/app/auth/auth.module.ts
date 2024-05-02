8ed82249c7bf36bfca898feff9fecfeda9447317
Vicent Paya <vicentpayaginer@gmail.com>
2024-05-02 17:57:00 +0200

    style: layout of the no-user page added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { IonicModule } from '@ionic/angular';
+import { NoUserPageComponent } from './pages/no-user-page/no-user-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NoUserPageComponent,
+    UserPageComponent,
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule,
+    IonicModule,
   ]
 })
 export class AuthModule { }
