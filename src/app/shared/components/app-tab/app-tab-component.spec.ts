558650cad073b4e4e859c2a416d20dec6b54c76b
Vicent Paya <vicentpayaginer@gmail.com>
2024-05-31 18:39:54 +0200

    tests: tests of shared module components added

---
+++ src/app/shared/components/app-tab/app-tab-component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { AppTabComponent } from './app-tab.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+
+fdescribe('RecomendationCard', () => {
+  let component: AppTabComponent;
+  let fixture: ComponentFixture<AppTabComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppTabComponent ],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of([{id: 1}]),
+          },
+        },
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppTabComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('setCurrentTab works correctly', () => {
+    component.setCurrentTab({tab:'home'});
+
+    expect(component.currentTab).toEqual('home');
+  });
+});
